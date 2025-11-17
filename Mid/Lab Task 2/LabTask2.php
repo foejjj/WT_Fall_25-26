c9b65810159de9b9c66bee4ff1882856f11bc23f
foejjj <22-49440-3@student.aiub.edu>
2025-11-17 13:13:12 +0600

    first commit

---
+++ Mid/Lab Task 2/LabTask2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Participate Registration</title>
+        <style>
+            body{
+                font-family: Arial, sans-serif;
+                background-color: #f0f8ff;
+                padding: 30px;
+            }
+        </style>
+    </head>
+</html>
