fe83b3d144cc0759a09684ebc1606fe3065145ef
foejjj <22-49440-3@student.aiub.edu>
2025-11-17 13:27:45 +0600

    2nd commit

--- Mid/Lab Task 2/LabTask2.php
+++ Mid/Lab Task 2/LabTask2.php
@@ -8,6 +8,19 @@
                 background-color: #f0f8ff;
                 padding: 30px;
             }
+            h2{
+                text-align: center;
+                color: #003366;
+            }
+            form, .box{
+                background-color: #ffffff;
+                padding: 20px;
+                border-radius: 10px;
+                width: 350px;
+                margin: 0 auto;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                margin-bottom: 20px;               
+            }
         </style>
     </head>
 </html>
