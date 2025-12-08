62f2a587cdf80742d86f129feabc40b49d4e96fd
shadat-ahmed-shovon <shadatashovon16@gmail.com>
2025-12-08 12:22:16 +0600

    2nd

--- Final/Lab Task 1.php
+++ Final/Lab Task 1.php
@@ -4,6 +4,13 @@ $toppingCount = 0;
 $orderComplete = false;
 
 echo "Choose your pizza type: <br>";
-echo "1. Magherita ($5) <br>";
+echo "1. Margherita ($5) <br>";
 echo "2. Pepperoni ($10) <br>";
 echo "3. Veggie Supreme ($15) <br>";
+
+$pizzaChoice = 2;
+
+switch($pizzaChoice){
+    case 1:
+        $pizzaType = "Margherita"
+}
