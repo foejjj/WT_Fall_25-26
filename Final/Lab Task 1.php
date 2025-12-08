1004353c775ae0ee7f1ca5566b454d74eb9df568
shadat-ahmed-shovon <shadatashovon16@gmail.com>
2025-12-08 12:18:19 +0600

    1st

---
+++ Final/Lab Task 1.php
@@ -0,0 +1,9 @@
+<?php
+$totalPrice = 0;
+$toppingCount = 0;
+$orderComplete = false;
+
+echo "Choose your pizza type: <br>";
+echo "1. Magherita ($5) <br>";
+echo "2. Pepperoni ($10) <br>";
+echo "3. Veggie Supreme ($15) <br>";
